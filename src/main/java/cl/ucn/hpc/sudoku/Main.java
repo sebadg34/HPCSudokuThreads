4eb01e1e201815bd5020882b611926c44c1959e7
Sebadg34 <Sebadg98@gmail.com>
2021-10-02 12:34:40 -0300

    Initial commit

---
+++ src/main/java/cl/ucn/hpc/sudoku/Main.java
@@ -0,0 +1,16 @@
+package cl.ucn.hpc.sudoku;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ *  The Main Class of Sudoku Solver
+ *
+ * @author Sebastian Delgado Guerra
+ */
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
